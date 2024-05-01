dd1d524991e811b3d91b9b36269c9db4fcbb7a96
AugustinCablant <123320681+AugustinCablant@users.noreply.github.com>
2024-05-01 10:00:01 +0200

    Update code.r

--- code.r
+++ code.r
@@ -6,5 +6,20 @@ install.packages("fUnitRoots") #Unit root test
 require(tseries) #functions for time series
 library(fUnitRoots)
 
+#DATASET DOWNLOAD (we use our Github repository)
+# getwd()
+setwd("/Users/augustincablant/Documents/GitHub/Time-Series")
 datafile <- "data.csv"
 data <- read.csv(datafile, sep=";")
+
+d <- length(data$Date)  #length of vector Date
+dates <- as.yearmon(seq(from=2024+2/12, to=1990, by=-1/12))  #vector from february 2024 to january 1990
+#indice
+class(data$Indice)
+
+#production
+production.source <- zoo(data$Indice, order.by=dates)
+T = length(data$Indice)
+production <- production.source[1:(T-4)]
+N <- length(production)
+dates_rev <- rev(dates[5:T])
