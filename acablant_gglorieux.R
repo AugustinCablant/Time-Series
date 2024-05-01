0411b63a2d6f79f86b9185054497b2271f80c700
Augustin Cablant <123320681+AugustinCablant@users.noreply.github.com>
2024-05-01 11:02:31 +0200

    Add files via upload

---
+++ acablant_gglorieux.R
@@ -0,0 +1,27 @@
+#INTRODUCTION
+# Website link : "https://www.insee.fr/fr/statistiques/serie/010767732#Documentation"
+
+#PACKAGES
+install.packages("fUnitRoots") #Unit root test
+install.packages("zoo")
+require(tseries) #functions for time series
+library(fUnitRoots)
+library(zoo)
+
+#DATASET DOWNLOAD 
+setwd("/Users/augustincablant/Desktop/Time Series project") #set your path
+datafile <- "data.csv"
+data <- read.csv(datafile, sep=";")
+
+d <- length(data$Date)  #length of vector Date
+dates <- as.yearmon(seq(from=2024+2/12, to=1990, by=-1/12))  #vector from february 2024 to january 1990
+
+#indice
+class(data$Indice)
+
+#production
+production.source <- zoo(data$Indice, order.by=dates)
+T = length(data$Indice)
+production <- production.source[1:(T-4)]
+N <- length(production)
+dates_rev <- rev(dates[5:T])
